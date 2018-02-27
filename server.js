c7d4cbfa48885412a1a20d0a4d5e02ba18667640
Elllie <bourgoishelene@gmail.com>
2018-02-27 10:26:52 +0100

    push server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const controller = require('./controllers/user')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+
 mongoose.connect('mongodb://localhost/myapp')
 mongoose.Promise = global.Promise
 
@@ -14,62 +15,6 @@ db.once('open', function(){
 })
 
 app.use(bodyParser.json())
-
-app.get('/user/:id', (req, res) => {
-    controller.find(req.params.id, (err, user) => {
-        // console.log(req.params.id)
-        if (err) {
-            res.status(500).send(err)
-        }
-       else {
-           res.status(200).send(user)
-       }
-    })
-    
-})
-
-app.post('/user', (req, res) => {
-    controller.create(req.query, (err, message) => {
-         // console.log(req.query)
-        if (err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send({message: message})
-        }
-    }) 
-})
-
-app.put('/user/:id', (req, res) => {
-    controller.update(req.params.id, req.query, (err) => {
-        if (err) {
-            res.status(500).send(err)
-
-        } else {
-            res.status(200).send({message: 'user modified well'})
-        }
-    })  
-})
-
-app.delete('/user/:id', (req, res) => {
-    controller.delete(req.params.id, (err) => {
-        if (err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send({message: 'user well deleted'})
-        }
-    })
-   
-})
-
-app.get('/login', (req, res) => {
-    controller.login(req.query, (err, message) => {
-        if (err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send({message: message})
-        }
-    })
- })
 
 
 app.listen(3000, () => {
