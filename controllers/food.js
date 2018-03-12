d33a4b9b77f216a4b9310fa455b207a1f80933b2
Elllie <bourgoishelene@gmail.com>
2018-03-12 18:22:20 +0100

    push food controller

--- controllers/food.js
+++ controllers/food.js
@@ -3,9 +3,11 @@ const Food = require('./../models/food')
 const moment = require('moment')
 const bcrypt = require('bcrypt')
 
+
 exports.create = (userId, food, callback) => { 
     food._user = userId
     let newFood = new Food(food)
+    newFood.createdAt = moment()
     newFood.save((err) => {
         callback(err, 'new food saved')
     
@@ -16,30 +18,19 @@ exports.find = (filter, callback) => {
     
     Food
     .find(filter)
-    .populate([
-        {
-            path: '_user',
-            select: '_profil',
-            populate: [
-                { 
-                    path: '_profil',
-                    select: 'experienceLevel'
-
-                }
-            ]
-            
-        }
-    ])
-
     .exec((err, foods) => {
         callback(err, foods)
     })
 }
     
+
 exports.update = (foodId, newFields, callback)  => {
+
     Food.findOne({ _id: foodId}, (err, food) => {
+        // console.log(user)
         food.set(newFields)
         food.save((err) => {
+            // console.log(user) 
            console.log('Food successfully updated !')
            callback(err)
        })
@@ -48,6 +39,7 @@ exports.update = (foodId, newFields, callback)  => {
 }
 
 exports.delete = (foodId, callback) => {
+// console.log(userId)
     if (!foodId) {
         return callback('right field necessary')
     }
