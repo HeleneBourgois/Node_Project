8b9edf8d731b8460bf821ebfc3008a4fec60aa2d
Elllie <bourgoishelene@gmail.com>
2018-03-20 09:42:47 +0100

    pushing config

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const userRouter = require('./userRouter.js')
-const foodRouter = require('./foodRouter.js')
-const recipeRouter = require('./recipeRouter.js')
-const profilRouter = require('./profilRouter.js')
-
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/myapp')
@@ -19,15 +11,3 @@ db.once('open', function(){
 })
 
 
-
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use('/', userRouter)
-app.use('/', foodRouter)
-app.use('/', recipeRouter)
-app.use('/', profilRouter)
-
-module.exports = userRouter
-module.exports = foodRouter
-module.exports = recipeRouter
-module.exports = profilRouter
